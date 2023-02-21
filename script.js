4f99a255353938be85e5911161b34f40206d7865
S2JuanS2 <juansdelrio@hotmail.com>
2023-02-21 13:07:01 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hola mundo");
+const mostrarReloj = ()=>{
+    let fecha = new Date();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+    document.getElementById("hora").innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+    const dias = ['Dom','Lun','Mar','Mie','Jue','Vie'];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let fechaTexto = `${diaSemana}, ${dia} ${mes}`;
+    document.getElementById("fecha").innerHTML = fechaTexto;
+
+    //document.getElementById("contenedor").classList.toggle("animar"); //activa o desactiva estilos toggle
+
+}
+
+const formatoHora = (hora)=>{
+    if(hora < 10){
+        hora = '0' + hora;
+    }
+    return hora;
+}
+
+
+setInterval(mostrarReloj, 1000);
