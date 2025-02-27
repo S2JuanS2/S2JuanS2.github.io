87e5de9909d2c2f3e72d5ddf49582d50553fe710
S2JuanS2 <juansdelrio@hotmail.com>
2025-02-27 18:43:16 -0300

    [New] image mant

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,12 @@ function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-grow justify-center items-center">
-        <div className="pt-10 border-4 border-blue-900 bg-blue-950 rounded-2xl w-10/12 h-80 text-center">
+        <div className="pt-10 border-4 border-blue-900 bg-blue-950 rounded-2xl w-10/12 h-60 text-center">
           <p className="text-white">Disculpa las molestias</p>
           <p className="text-white">El sitio esta actualmente bajo mantenimiento</p>
+          <div className="flex justify-center">
+            <img src="/images/mantenimiento.png" alt="mant" className="mt-5"/>
+          </div>
         </div>
       </div>
       <Footer className="mt-auto" />
