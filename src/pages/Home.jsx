66fab229c0254745a9252bc9f957424f6b4b0d27
S2JuanS2 <juansdelrio@hotmail.com>
2025-03-01 14:00:23 -0300

    [Fix]: Corrección de errores visuales

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home (){
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowButton(true);
-        }, 12000);
+        }, 8000);
 
         return () => clearTimeout(timer);
     }, []);
@@ -22,20 +22,19 @@ function Home (){
                 <div className="pt-8 border-4 border-blue-900 bg-blue-950 rounded-2xl w-10/12 h-100 text-center">
                     <h1 className="text-white font-bold underline underline-offset-2"><TypewriterEffect text={"Bienvenido a Thinks & Pick!\n"} speed={50} delay={0}></TypewriterEffect></h1>
                     <p className="text-white"><TypewriterEffect text={"El sitio se encuentra en desarrollo."} speed={50} delay={2}></TypewriterEffect></p>
-                    <p className="text-white"><TypewriterEffect text={"API alojada en un servidor gratuito."} speed={50} delay={5}></TypewriterEffect></p>
-                    <h2 className="text-white font-bold underline underline-offset-2 mt-5"><TypewriterEffect text={"Leyenda"} speed={50} delay={8}></TypewriterEffect></h2>
+                    <h2 className="text-white font-bold underline underline-offset-2 mt-5"><TypewriterEffect text={"Leyenda"} speed={50} delay={5}></TypewriterEffect></h2>
                     <div className="flex flex-col justify-center ml-20 mt-4 space-y-2">
                         <div className="flex">
                             <img src="/images/info.png" alt="info" className="w-6 h-6"/>
-                            <p className="text-white ml-2"><TypewriterEffect text={"Ayuda."} speed={50} delay={9}></TypewriterEffect></p>
+                            <p className="text-white ml-2"><TypewriterEffect text={"Información."} speed={50} delay={6}></TypewriterEffect></p>
                         </div>
                         <div className="flex">
                             <img src="/images/user-img.png" alt="user" className="w-6 h-6"/>
-                            <p className="text-white ml-2"><TypewriterEffect text={"Repositorio del proyecto."} speed={50} delay={10}></TypewriterEffect></p>
+                            <p className="text-white ml-2"><TypewriterEffect text={"Origen."} speed={50} delay={7}></TypewriterEffect></p>
                         </div>
                     </div>
                     {showButton && (
-                        <div className="mt-9">
+                        <div className="mt-10">
                             <a href="/#/thinks-pick" className="text-white border-2 border-cyan-500 font-bold p-1 rounded-lg bg-cyan-500 hover:border-blue-300 hover:text-blue-950 transition-colors duration-300">EMPEZAR</a>
                         </div>
                     )}
