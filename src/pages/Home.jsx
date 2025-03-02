1aab24eb7df29120c919404e19c42849d5e9630b
S2JuanS2 <juansdelrio@hotmail.com>
2025-03-02 02:00:58 -0300

    update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,15 +22,14 @@ function Home (){
                 <div className="pt-8 border-4 border-blue-900 bg-blue-950 rounded-2xl w-10/12 h-100 text-center">
                     <h1 className="text-white font-bold underline underline-offset-2"><TypewriterEffect text={"Bienvenido a Thinks & Pick!\n"} speed={50} delay={0}></TypewriterEffect></h1>
                     <p className="text-white"><TypewriterEffect text={"El sitio se encuentra en desarrollo."} speed={50} delay={2}></TypewriterEffect></p>
-                    <h2 className="text-white font-bold underline underline-offset-2 mt-5"><TypewriterEffect text={"Leyenda"} speed={50} delay={5}></TypewriterEffect></h2>
                     <div className="flex flex-col justify-center ml-20 mt-4 space-y-2">
                         <div className="flex">
                             <img src="/images/info.png" alt="info" className="w-6 h-6"/>
-                            <p className="text-white ml-2"><TypewriterEffect text={"Información."} speed={50} delay={6}></TypewriterEffect></p>
+                            <p className="text-white ml-2"><TypewriterEffect text={"Información."} speed={50} delay={5}></TypewriterEffect></p>
                         </div>
                         <div className="flex">
                             <img src="/images/user-img.png" alt="user" className="w-6 h-6"/>
-                            <p className="text-white ml-2"><TypewriterEffect text={"Origen."} speed={50} delay={7}></TypewriterEffect></p>
+                            <p className="text-white ml-2"><TypewriterEffect text={"Github."} speed={50} delay={6}></TypewriterEffect></p>
                         </div>
                     </div>
                     {showButton && (
