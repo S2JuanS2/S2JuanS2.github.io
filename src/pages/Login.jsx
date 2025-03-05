c57eac45c1f017bb83e2c2a225d4dc824674216c
S2JuanS2 <juansdelrio@hotmail.com>
2025-03-05 16:06:44 -0300

    Links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,13 +27,31 @@ const Login = () => {
       <div className="flex flex-col min-h-screen">
         <Header></Header>
         <div className="flex flex-col text-center">
-            {error && <p style={{ color: "red" }}>{error.message}</p>}
         </div>
         <form onSubmit={handleLogin} className="text-white">
             <div className="mt-40 flex flex-col m-6 items-center border-1 border-blue-800 rounded-2xl bg-blue-950 p-4 space-y-2">
-                <h2 className="text-white font-bold">iniciar sesión</h2>
-                <input className="border-b-1 p-1" type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <input className="border-b-1 p-1" type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <h2 className="text-white font-bold mb-5">iniciar sesión</h2>
+                <input 
+                  className="border-b-1 p-1" 
+                  type="text" 
+                  placeholder="Usuario" 
+                  value={username}
+                  required
+                  onChange={(e) => setUsername(e.target.value)} 
+                />
+                <input 
+                  className="border-b-1 p-1" 
+                  type="password" 
+                  placeholder="Contraseña"
+                  required
+                  value={password} onChange={(e) => setPassword(e.target.value)} 
+                />
+              { error ? (
+                  <p className="text-red-500">Error al iniciar sesión</p>
+                ) : (
+                  <p className="p-3"> </p>
+                )}
+                <a href="/#/register" className="text-sm underline underline-offset-2">Registrarse</a>
                 <button className="mt-4 pl-4 pr-4 cursor-pointer border-1 bg-blue-500 rounded-sm text-white border-blue" type="submit">Ingresar</button>
             </div>
         </form>
