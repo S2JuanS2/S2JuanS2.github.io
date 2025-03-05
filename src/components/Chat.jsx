239bae1fb191e0e6892371dd93683f698af6d571
S2JuanS2 <juansdelrio@hotmail.com>
2025-03-05 19:21:32 -0300

    Visual chat fixed

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -32,32 +32,35 @@ const Chat = () => {
   };
 
   return (
-    <div className="flex flex-col p-4 mt-5 ml-5 mr-5 mx-auto bg-blue-900 rounded-lg">
-        <div className="overflow-auto h-64 mb-4">
-        {messages.map((msg, index) => {
-            const [userName, userMessage] = msg.split(': ');
-            return (
-            <div key={index} className="p-2 pl-4 mb-2 bg-gray-100 rounded-lg mr-4 ml-4">
-            <span className="font-bold">{userName}:</span> {userMessage}
-            </div>
-            );
-        })}
+    <div className="flex flex-col w-auto p-4 mt-5 ml-5 mr-5 mx-auto bg-gray-600 border-black border-2 rounded-lg">
+        <div className='border-2 rounded-sm pt-4 border-black bg-gray-800 mb-3'>
+          <div className="overflow-auto h-64 mb-4">
+          {messages.map((msg, index) => {
+              const [userName, userMessage] = msg.split(': ');
+              return (
+              <div key={index} className="pt-1 pl-2 pr-2 mb-2 bg-gray-100 rounded-lg mr-4 ml-4 h-7 w-max">
+              <span className="font-bold">{userName}:</span> {userMessage}
+              </div>
+              );
+          })}
+          </div>
         </div>
         <input
             type="text"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className="p-2 border rounded-lg mb-2 text-white"
+            className="pl-1 pr-1 pb-1 border-b-1 rounded-sm text-white mb-4"
             placeholder="Escribe un mensaje"
+            maxLength={32}
         />
-        <div className='flex justify-end'>
+        <div className='text-end'>
             <input 
                 type="text" 
                 value={userName}
                 readOnly
-                className='border-1 text-white rounded-l-lg pl-4 text-end pr-4 font-bold w-full'
+                className="text-white pl-4 text-end pr-4 font-bold w-max"
             />
-            <button onClick={sendMessage} className="px-2 py-1 border-1 bg-blue-500 text-white rounded-r-lg w-30 cursor-pointer hover:border-white">
+            <button onClick={sendMessage} className="px-4 py-1 border-1 bg-gray-400 text-white rounded-r-lg w-auto cursor-pointer hover:border-white">
                 Enviar
             </button>
         </div>
